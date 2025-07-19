12d9422e4afad43992b8679bec24317954eda7c2
progsherief <progsherief@gmail.com>
2025-07-19 08:35:07 +0300

    added ProductsByCategorySpecification to filter products by category

---
+++ ProductCatalog/Domain/Specifications/ProductSpecifications/ProductsByCategorySpecification.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Specifications.ProductSpecifications;
+
+public class ProductsByCategorySpecification:BaseSpecification<Product>
+{
+    public ProductsByCategorySpecification(Guid categoryId)
+    {
+        AddCriteria(p => p.CategoryId == categoryId);
+        AddInclude(p => p.Category);
+        ApplyOrderByDescending(p => p.CreatedAt);
+    }
+}
