e2ec48b57f189d8eda60cbcee22fe0b453068638
progsherief <progsherief@gmail.com>
2025-07-19 08:31:55 +0300

    added ProductsAvailableNowSpecification to filter active products

---
+++ ProductCatalog/Domain/Specifications/ProductSpecifications/ProductsAvailableNowSpecification.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Domain.Specifications.ProductSpecifications;
+
+public class ProductsAvailableNowSpecification:BaseSpecification<Product>
+{
+    public ProductsAvailableNowSpecification()
+    {
+        var now = DateTime.UtcNow;
+
+        AddCriteria(p =>
+            p.StartDate <= now &&
+            now <= p.StartDate.AddDays(p.DurationInDays));
+
+        AddInclude(p => p.Category); 
+        ApplyOrderByDescending(p => p.CreatedAt);
+    }
+}
