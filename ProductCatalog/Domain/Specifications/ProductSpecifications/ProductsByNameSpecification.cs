5b59b26e30dbdedfb11f612c461d7d7fdc47abed
progsherief <progsherief@gmail.com>
2025-07-19 08:36:13 +0300

    added ProductsByNameSpecification for name-based product search

---
+++ ProductCatalog/Domain/Specifications/ProductSpecifications/ProductsByNameSpecification.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Specifications.ProductSpecifications;
+
+public class ProductsByNameSpecification:BaseSpecification<Product>
+{
+    public ProductsByNameSpecification(string name)
+    {
+        var lowerName = name.ToLower();
+
+        AddCriteria(p => p.Name.ToLower().Contains(lowerName));
+        AddInclude(p => p.Category);
+        ApplyOrderBy(p => p.Name);
+    }
+}
